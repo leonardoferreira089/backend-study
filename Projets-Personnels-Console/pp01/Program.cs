f4496872bc5efd121b6f4b9ab730ebd0627a8059
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2025-09-04 21:10:34 +0200

    feat : Projets perso console

---
+++ Projets-Personnels-Console/pp01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double dinheiro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double produto = 7.75;
+
+        double troco = dinheiro - produto;
+        Console.WriteLine($"Seu troco é de {troco:F2}");
+
+    }
+}
---
+++ Projets-Personnels-Console/pp02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o Valor A : ");
+        int A = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o Valor B : ");
+        int B = int.Parse(Console.ReadLine());
+        int resultado = A * B;
+
+
+        Console.WriteLine($"A multiplicação desses números é " + resultado);
+
+    }
+}
