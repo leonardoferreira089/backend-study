a893139c18dce3cbbc0ed274882c658058b3ae19
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2025-09-04 20:56:39 +0200

    feat : exercise 1002

---
+++ Exercices-Console/Beecrowd/Beecrowd_1002/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double area = 3.14159 * (raio * raio);
+        Console.WriteLine($"A={area:F4}");
+
+    }
+}
