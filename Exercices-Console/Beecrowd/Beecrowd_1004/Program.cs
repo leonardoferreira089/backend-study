7695a22b01585f42ed0e8b01aac89e5aa859bfb8
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2025-09-16 21:49:26 +0200

    feat beecrowd 1004

---
+++ Exercices-Console/Beecrowd/Beecrowd_1004/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        int PROD = A * B;
+
+        Console.WriteLine("PROD = " + PROD );
+
+    }
+
+}
