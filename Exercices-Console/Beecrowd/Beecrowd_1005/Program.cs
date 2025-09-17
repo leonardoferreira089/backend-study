50a0b667b06766e014f89f602a515386ae1cc453
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2025-09-17 22:00:45 +0200

    feat beecrowd 1005

---
+++ Exercices-Console/Beecrowd/Beecrowd_1005/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double Media = (a * 3.5 + b * 7.5) / 11.0;
+
+        Console.WriteLine("MEDIA = " + Media.ToString("F5", CultureInfo.InvariantCulture));
+    }
+
+}
