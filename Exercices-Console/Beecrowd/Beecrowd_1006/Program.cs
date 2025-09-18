0dc4f9e3ec53fef823a0aee4ff5009c7cdacc150
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2025-09-18 21:15:55 +0200

    feat beecrowd 1006

---
+++ Exercices-Console/Beecrowd/Beecrowd_1006/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double MEDIA = (A * 2 + B * 3 + C * 5) / 10.0;
+
+        Console.WriteLine("MEDIA = " + MEDIA.ToString("F1", CultureInfo.InvariantCulture));
+
+    }
+
+}
